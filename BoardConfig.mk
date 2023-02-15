b998b572dcb75ac001ffc95937e7a4af2663e6b1
Vhmit <praxedes.9630@gmail.com>
2023-02-15 13:49:52 -0300

    j4corelte: Import kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,5 +14,17 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
+# Kernel
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive loop.max_part=7
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_CONFIG := j4corelte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+ARCH_ARM_HAVE_NEON := true
+
 # Inherit the proprietary files
 include vendor/samsung/j4corelte/BoardConfigVendor.mk
