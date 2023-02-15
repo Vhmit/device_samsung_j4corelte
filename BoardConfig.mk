6739078c1e18e298e043350fcd3066383f090c66
Vhmit <praxedes.9630@gmail.com>
2023-02-15 13:47:47 -0300

    j4corelte: Import architecture flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,13 @@
 
 DEVICE_PATH := device/samsung/j4corelte
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
 # Inherit the proprietary files
 include vendor/samsung/j4corelte/BoardConfigVendor.mk
